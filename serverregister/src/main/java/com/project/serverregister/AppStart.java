e2b5ebeff9e0635bfbcc8bc28759945e6d00de7f
Aswin K <94516249+ImpossibleAk@users.noreply.github.com>
2025-05-18 13:53:30 +0530

    Add files via upload

---
+++ serverregister/src/main/java/com/project/serverregister/AppStart.java
@@ -0,0 +1,31 @@
+package com.project.serverregister;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.project.serverregister.config.AppConfig;
+import com.project.serverregister.form.FrmMainScreen;
+
+@Configuration
+@ComponentScan("com.project.serverregister.*")
+public class AppStart {
+
+	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+
+		UIManager.setLookAndFeel(new FlatIntelliJLaf());
+		UIManager.put("Button.arc", 15); // Rounded corners for buttons
+		UIManager.put("Component.arc", 20); // Rounded corners for other components
+		UIManager.put("TextComponent.arc", 20); // Rounded corners for text fields
+
+		AppConfig.context = new AnnotationConfigApplicationContext(AppStart.class);
+
+		FrmMainScreen frmMain = AppConfig.context.getBean(FrmMainScreen.class);
+		frmMain.setVisible(true);
+	}
+
+}
---
+++ serverregister/src/main/java/com/project/serverregister/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.project.serverregister.config;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+
+	public static AbstractApplicationContext context;
+
+}
